532445cd756b227c931535c957f768e5141de0b0
Sarah <83969766+sgeorge22@users.noreply.github.com>
2021-06-16 09:20:07 -0400

    Add project files.

---
+++ PigDiceGame/PigDice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigDiceGame
+{
+    class PigDice
+    {
+        Random r = new Random(); // random class is a psuedo random number generator
+
+        public int[] Play(int ScoreToBeat = 0)
+        {
+            //var NumberOfGames = 0;
+            var HighScore = 0;
+            var GamesPlayed = 0;
+            while(HighScore <= ScoreToBeat)
+            {
+                GamesPlayed++;
+                var score = PlayOneGame();
+                if(score > HighScore)
+                {
+                    HighScore = score;
+                }
+            }
+            return new int[] { HighScore, GamesPlayed };
+        }
+        int PlayOneGame()
+        {
+            var score = 0;
+            var die = Roll();
+            while (die != 1)
+            {
+                score += die;
+                die = Roll();
+            }
+            return score;
+        }
+        int Roll()
+        {
+            return r.Next(1, 7);
+        }
+    }
+}
---
+++ PigDiceGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PigDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pigdice = new PigDice();
+            int[] results = pigdice.Play(400);
+            Console.WriteLine($"High score is {results[0]} for {results[1]} games played!");
+        }
+    }
+}
